60d295b6ec2a386af74ff419efadb02953ce7d21
zwill <zwill_1@hotmail.com>
2024-05-15 23:12:24 -0600

    styles of product tags and pages

--- assets/products-page-tags.php
+++ assets/products-page-tags.php
@@ -1,10 +1,22 @@
-<section class="container-fluid">
+<section class="container-fluid bg-body-tertiary">
     <div class="row">
-        <div class="col-2 bg-info text-center p-2 border border-black pe-auto" role="button"><a href="#" class="text-black fst-italic text-decoration-none">shoes</a></div>
-        <div class="col-2 bg-info text-center p-2 border border-black pe-auto" role="button"><a href="#" class="text-black fst-italic text-decoration-none">shoes</a></div>
-        <div class="col-2 bg-info text-center p-2 border border-black pe-auto" role="button"><a href="#" class="text-black fst-italic text-decoration-none">shoes</a></div>
-        <div class="col-2 bg-info text-center p-2 border border-black pe-auto" role="button"><a href="#" class="text-black fst-italic text-decoration-none">shoes</a></div>
-        <div class="col-2 bg-info text-center p-2 border border-black pe-auto" role="button"><a href="#" class="text-black fst-italic text-decoration-none">shoes</a></div>
-        <div class="col-2 bg-info text-center p-2 border border-black pe-auto" role="button"><a href="#" class="text-black fst-italic text-decoration-none">shoes</a></div>
+        <div class="col-2 text-center p-2 pe-auto py-2" role="button">
+            <a href="http://localhost/PHP-PRODUCT-UPLOAD-v2/products/new-arrivals.php" class="text-black fst-italic text-decoration-none">New Arrivals <i class="ri-arrow-right-s-line"></i></a>
+        </div>
+        <div class="col-2 text-center p-2 pe-auto py-2" role="button">
+            <a href="http://localhost/PHP-PRODUCT-UPLOAD-v2/products/shirts.php" class="text-black fst-italic text-decoration-none">Shirts <i class="ri-arrow-right-s-line"></i></a>
+        </div>
+        <div class="col-2 text-center p-2 pe-auto py-2" role="button">
+            <a href="http://localhost/PHP-PRODUCT-UPLOAD-v2/products/pants.php" class="text-black fst-italic text-decoration-none">Pants <i class="ri-arrow-right-s-line"></i></a>
+        </div>
+        <div class="col-2 text-center p-2 pe-auto py-2" role="button">
+            <a href="http://localhost/PHP-PRODUCT-UPLOAD-v2/products/shorts.php" class="text-black fst-italic text-decoration-none">Shorts <i class="ri-arrow-right-s-line"></i></a>
+        </div>
+        <div class="col-2 text-center p-2 pe-auto py-2" role="button">
+            <a href="http://localhost/PHP-PRODUCT-UPLOAD-v2/products/shoes.php" class="text-black fst-italic text-decoration-none">Shoes <i class="ri-arrow-right-s-line"></i></a>
+        </div>
+        <div class="col-2 text-center p-2 pe-auto py-2" role="button">
+            <a href="http://localhost/PHP-PRODUCT-UPLOAD-v2/products/sales.php" class="text-black fst-italic text-decoration-none">Sales <i class="ri-arrow-right-s-line"></i></a>
+        </div>
     </div>
 </section>
