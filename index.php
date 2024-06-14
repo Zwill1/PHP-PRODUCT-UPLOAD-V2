a14db2c2134a775b3746dd3c3f505234a3fd859e
zwill <zwill_1@hotmail.com>
2024-06-13 23:01:27 -0600

    finish card style for index page

--- index.php
+++ index.php
@@ -130,24 +130,26 @@
             ?>
 
             <?php foreach ($results as $row): ?>
-                    <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 p-2">
-                        <div class="card border-0 p-2 rounded-0 bg-body-secondary py-4" id="product-<?php echo htmlspecialchars($row['prodid']); ?>">
-                            <div class="d-flex justify-content-center">
-                                <img src="<?php echo htmlspecialchars($row['prodimage']); ?>" class="card-img-top" alt="<?php echo htmlspecialchars($row['prodname']); ?>" style="height: 150px; width: auto;">
-                            </div>
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <h5 class="card-title mb-1 fw-bold"><?php echo htmlspecialchars($row['prodname']); ?></h5>
+                    <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 p-0">
+                        <div class="border rounded-none border-8 border-solid border-gray-800 m-2">
+                            <div class="card px-2 rounded-0 py-3 border-transparent" id="product-<?php echo htmlspecialchars($row['prodid']); ?>">
+                                <div class="d-flex justify-content-center">
+                                    <img src="<?php echo htmlspecialchars($row['prodimage']); ?>" class="card-img-top" alt="<?php echo htmlspecialchars($row['prodname']); ?>" style="height: 150px; width: auto;">
                                 </div>
-                                <div class="d-flex justify-content-evenly mt-3">
-                                        <p>5 <span style="color:#ffa41c">&#9733;&#9733;&#9733;&#9733;&#9733;</span></p>
-                                        <p class="ms-3"><?php echo htmlspecialchars($row['prodreviewcount']); ?> ratings</p>
-                                </div>
-                                <div class="text-center"">
-                                    <p class="card-text mb-3 fw-bold fs-5">$<?php echo htmlspecialchars($row['prodprice']); ?></p>
-                                </div>
-                                <div class="col-12">
-                                    <a href="db/product.php?id=<?php echo htmlspecialchars($row['prodid']); ?>" class="btn btn-info w-100">Details</a>
+                                <div class="card-body">
+                                    <div class="text-center">
+                                        <h5 class="card-title mb-1 fw-bold"><?php echo htmlspecialchars($row['prodname']); ?></h5>
+                                    </div>
+                                    <div class="d-flex justify-content-evenly mt-3">
+                                            <p>5 <span style="color:#ffa41c">&#9733;&#9733;&#9733;&#9733;&#9733;</span></p>
+                                            <p class="ms-3"><?php echo htmlspecialchars($row['prodreviewcount']); ?> ratings</p>
+                                    </div>
+                                    <div class="text-center"">
+                                        <p class="card-text mb-3 fw-bold fs-5">$<?php echo htmlspecialchars($row['prodprice']); ?></p>
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="db/product.php?id=<?php echo htmlspecialchars($row['prodid']); ?>" class="btn btn-info w-100">Details</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
