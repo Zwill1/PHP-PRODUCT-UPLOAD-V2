82d50fc01814b5d1564e99007e6bc708182327eb
zwill <zwill_1@hotmail.com>
2024-06-23 19:48:58 -0600

    changing styles on top header

--- assets/top-header.php
+++ assets/top-header.php
@@ -1,4 +1,4 @@
-<div class="container-fluid text-bg-primary">
+<div class="container-fluid text-bg-primary p-0">
     <div class="container px-4">
         <div class="row">
             <p class="text-center mb-1 text-uppercase fw-bold">Free US shipping on all orders</p>
