cf97f46b76f349bedf658a8a9b1b46404e865dea
zwill <zwill_1@hotmail.com>
2024-06-23 19:49:36 -0600

    adding credit card logos and style changes to footer

--- assets/footer.php
+++ assets/footer.php
@@ -1,12 +1,7 @@
-    <footer class="bg-body-tertiary pb-3 pt-3 mt-5">
-        <div class="container mt-5">
-            <div class="row">
-                <div class="col-sm-12 col-md-4 text-center text-sm-center text-md-start">
-                    <a class="fw-bold" href="http://localhost/PHP-PRODUCT-UPLOAD/">
-                        <img src="http://localhost/PHP-PRODUCT-UPLOAD/assets/product-upload-logo.png" width="200px" alt="Product Upload logo" />
-                    </a>
-                </div>
-                <div class="col-sm-12 col-md-4 text-center text-sm-center text-md-start">
+    <footer class="bg-body-tertiary pb-3 pt-3">
+        <div class="container border-bottom">
+            <div class="row py-4">
+                <div class="col-sm-12 col-md-3 text-center text-sm-center text-md-start">
                     <h6 class="fw-bold">Address</h6>
                     <p class="mb-0">Product Upload Ltd.</p>
                     <p class="mb-0">111 Calgary Ave.</p>
@@ -14,10 +9,47 @@
                     <p class="fw-bold">Phone <a href="#" class="text-decoration-none">(587) 111-2222</a></p>
                     <p class="fw-bold">Email <a href="#" class="text-decoration-none">info@productupload.ca</a></p>
                 </div>
-                <div class="col-sm-12 col-md-4 text-center text-sm-center text-md-start">
-                    <h6 class="fw-bold">Hours:</h6>
-                    <p class="mb-0">Monday - Saturday</p>
-                    <p>11 am to 5:30 pm</p>
+                <div class="col-sm-12 col-md-3 text-center text-sm-center text-md-start">
+                    <h6 class="fw-bold">Store Locations</h6>
+                    <p class="mb-0">Calgary - CANADA</p>
+                    <p class="mb-0">Airdrie - CANADA</p>
+                    <p class="mb-0">Balzac - CANADA</p>
+                    <p class="mb-0">Cochrane - CANADA</p>
+                    <p class="mb-0">Banff - CANADA</p>
+                    <p class="mb-0">Academy - CANADA</p>
+                </div>
+                <div class="col-sm-12 col-md-3 text-center text-sm-center text-md-start">
+                    <div>
+                        <h6 class="fw-bold">Store Hours:</h6>
+                        <p class="mb-0">Monday - Saturday</p>
+                        <p class="mb-0">11 am to 5:30 pm</p>
+                        <p>Sunday - Closed</p>
+                    </div>
+                    <div>
+                        <h6 class="fw-bold">Customer Service:</h6>
+                        <p class="mb-0">Returns</p>
+                        <p class="mb-0">Support</p>
+                        <p class="mb-0">Customer Feedback</p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-3 text-center text-sm-center text-md-start">
+                    <h6 class="fw-bold">Information:</h6>
+                    <p class="mb-0">Affiliates</p>
+                    <p class="mb-0">Brands</p>
+                    <p class="mb-0">Gift Certificates</p>
+                    <p class="mb-0">Specials</p>
+                    <p class="mb-0">Product Support</p>
+                    <p class="mb-0">New Stock</p>
+                </div>
+            </div>
+        </div>
+        <div class="container mt-3">
+            <div class="row d-flex align-items-center">
+                <div class="w-full col-lg-6 text-center text-lg-start">
+                    <p class="mb-0">© 2024 Product Upload. All Rights Reserved.</p>
+                </div>
+                <div class="w-full col-lg-6 text-center text-lg-end">
+                    <img src="http://localhost/PHP-PRODUCT-UPLOAD-v2/assets/images/payment-icons.png" alt="" />
                 </div>
             </div>
         </div>
