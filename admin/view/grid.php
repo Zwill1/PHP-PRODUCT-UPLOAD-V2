f73139ecafda1a2bb176010509504df83f6218ed
zwill <zwill_1@hotmail.com>
2024-07-05 00:01:52 -0600

    corrections to page links for add product

--- admin/view/grid.php
+++ admin/view/grid.php
@@ -113,7 +113,7 @@
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
-            <form action="../db/create_product.php" method="POST">
+            <form action="../../db/create_product.php" method="POST">
                 <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Add New Product</h5>
