df77c0489890cbc5895d610ac7af6fa009bff794
zwill <zwill_1@hotmail.com>
2024-07-09 00:54:36 -0600

    adding exit and changing up link to account page

--- db/delete_product.php
+++ db/delete_product.php
@@ -2,18 +2,20 @@
 
 <?php
 
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
-}
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+    }
 
     $query = "delete from `products` where `prodid` = :id";
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 
     if($stmt->execute()){
-        header("location:../index.php?delete_msg=The product has been deleted to the database.");
+        header("location:../admin/account.php?delete_msg=The product has been deleted to the database.");
+        exit;
     }else{
-        echo "Something went wrong. Please try again.";
+        header("location:../admin/account.php?message=Their was an error deleting the product.");
+        exit;
     }
     
 ?>
